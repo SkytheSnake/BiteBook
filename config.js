656e751dfbc5b56529b3d708a1d0726306eaa7a5
Sky Walker <skythesnake95@gmail.com>
2026-08-12 16:11:30 +0100

    Change Google Maps API key in config.js
    Updated Google Maps API key in configuration.

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // This key should be restricted to your GitHub Pages URL and the APIs BiteBook uses.
 
 window.BITEBOOK_CONFIG = {
-  googleMapsApiKey: 'PASTE_YOUR_GOOGLE_MAPS_API_KEY_HERE'
+  googleMapsApiKey: 'AIzaSyDa9RxDK-fm3bOKJz5ISk9LjWtiQQY5vUA'
 };
